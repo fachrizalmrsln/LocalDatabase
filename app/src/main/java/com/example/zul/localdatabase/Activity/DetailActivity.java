1bfc01253dd9b4a7ff8186d3d13a50dd25013f43
zul <fahrizalmursalin06@gmai.com>
2019-01-18 21:43:01 +0700

    initial commit

--- app/src/main/java/com/example/zul/localdatabase/Activity/DetailActivity.java
+++ app/src/main/java/com/example/zul/localdatabase/Activity/DetailActivity.java
@@ -1,8 +1,8 @@
 package com.example.zul.localdatabase.Activity;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
